de5af4229b059fb45b341a05c0910313db991d82
Jason Nestmann <nestmannj@bellsouth.net>
2017-04-24 14:43:28 -0400

    Added additional graphics and started OOP for game objects.

---
+++ GameObjects.py
@@ -0,0 +1,19 @@
+class GameObjects:
+    def __init__(self, x=0, y=0, width=0, height=0, img=""):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.img = img
+
+
+class Penguin(GameObjects):
+    pass
+
+
+class Snowball(GameObjects):
+    pass
+
+
+class Obstacle(GameObjects):
+    pass
--- game.py
+++ game.py
@@ -1,49 +1,65 @@
-
-import pygame, sys
+import GameObjects
+import pygame
+import sys
 import random
-pygame.init()
 
-size = width, height = 620,320
-color = 208,236,253
-black = 0,0,0
-white = 255,255,255
-red = 255,2,2
-screen = pygame.display.set_mode(size)
+pygame.init()
 clock = pygame.time.Clock()
 
-penguin_width = 34
-penguin_height = 34
-obstacle_width = 57
+
+# delete as soon as possible
 obstacle_height = 52
-snowball_width = 34
-snowball_height = 34
+obstacle_width = 57
 
+
+bg_filenames = ('assets/images/bg_title.jpg',
+                'assets/images/bg_game.jpg',
+                'assets/images/bg_credits.jpg'
+                )
+
+
+# setup game display
+bg_northpole = pygame.image.load(bg_filenames[1])
+size = width, height = 620*2,320*2
+screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Penguin')
+pygame.display.set_icon(pygame.image.load('assets/images/penguin_icon.png').convert_alpha())
 
-#background
-northpole = pygame.image.load("assets/images/polar.png")
+# Set Color Constants
+PALE_BLUE = 208, 236, 253
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 2, 2)
+
+
+penguin_img = pygame.image.load('assets/images/small_penguin.png')
+penguin = GameObjects.Penguin(width=34,height=34,img=penguin_img)
+
+obstacle_img = pygame.image.load('assets/images/obstacle1.png')
+obstacle = GameObjects.Obstacle(width=57, height=52, img=obstacle_img)
+
+snowball_img = pygame.image.load('assets/images/small_snowball.png').convert_alpha()
+snowball = GameObjects.Snowball(width=34, height=34, img=snowball_img)
+
+
 
 #obstacle
-obstacle = pygame.image.load("assets/images/obstacle1.png")
-smallobstacle = pygame.transform.scale(obstacle,(obstacle_width,obstacle_height))
-obstaclerect= smallobstacle.get_rect()
-#SNOW BALL
-snowball= pygame.image.load("assets/images/snowball-icon.png")
-smallball = pygame.transform.scale(snowball,(snowball_width,snowball_height))
-snowballrect = smallball.get_rect()
-#snowball.set_colorkey(black)
+#obstaclerect= smallobstacle.get_rect()
 
-#penguin 
-penguin = pygame.image.load('assets/images/penguin2.png')
-smallpenguin= pygame.transform.scale(penguin,(penguin_width,penguin_height))
-penguinrect = smallpenguin.get_rect()
-smallpenguin.set_colorkey(white)
+#snowball= pygame.image.load("assets/images/snowball-icon.png")
+#smallball = pygame.transform.scale(snowball,(snowball_width,snowball_height))
+#snowballrect = smallball.get_rect()
+#snowball.set_colorkey(BLACK)
+
+def pquit():
+    pygame.quit()
+    exit()
 
 def background(xpol,ypol):
-    screen.blit(northpole,(xpol,ypol))
+    screen.blit(bg_northpole,(xpol,ypol))
 
 def ball(x1,y2):
-    screen.blit(smallball,(x1,y2))
+    screen.blit(snowball_img,(x1,y2))
 
 def game_over():
     font = pygame.font.SysFont(None,80)
@@ -52,10 +68,10 @@ def game_over():
     screen.blit(message,(165,100))
 
 def obstacles(xloc,yloc):
-    screen.blit(smallobstacle,(xloc,yloc))
+    screen.blit(obstacle.img,(xloc,yloc))
     
 def pin(x,y):
-    screen.blit(smallpenguin,(x,y))
+    screen.blit(penguin.img,(penguin.x, penguin.y))
 
 def game_loop():
     
@@ -87,35 +103,35 @@ def game_loop():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                
-                pygame.quit()
-                sys.exit()
+                pquit()
 
-                #check for keys being pressed by user 
             if event.type == pygame.KEYDOWN:
-                #check fo the left arrow key
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_ESCAPE:
+                    pquit()
+                elif event.key == pygame.K_LEFT:
                     x_change = -5
-                if event.key == pygame.K_RIGHT:
-                    x_change = 5 
-                if event.key == pygame.K_UP:
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+                elif event.key == pygame.K_UP:
                     y_change = -5
-                if event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN:
                     y_change = 5
+
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
+
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     y_change = 0
                 
          
-        x += x_change
-        y += y_change
+        penguin.x += x_change
+        penguin.y += y_change
 
 
         
-        screen.fill(color)
+        screen.fill(PALE_BLUE)
         background(xpol,ypol)
         pin(x,y)
         ball(x1,y2)
@@ -125,14 +141,14 @@ def game_loop():
         y2 -= ballspeed
         
         #check if penguin crashes on top or at the bottom
-        if y > 323 - penguin_height or y<0:
+        if penguin.y > 323 - penguin.height or penguin.y < 0:
             game_over()
             y_change = 0
             obspeed = 0
             ballspeed = 0
         #check for obstacles
         if y > yloc-obstacle_height:
-            if (x > xloc and x<xloc-obstacle_width) or (x + penguin_width > xloc -penguin_width<xloc - obstacle_width):
+            if (x > xloc and x<xloc-obstacle_width) or (x + penguin.width > xloc - penguin.width < xloc - obstacle_width):
                 game_over()
 
         
@@ -145,9 +161,9 @@ def game_loop():
         clock.tick(60)
         pygame.display.flip()
 
+    pquit()
+
 game_loop()
-pygame.quit()
-quit()
 
 
 
