f92cf814d1f275e4dda396611ef36d38749ec0b5
Jason Nestmann <nestmannj@bellsouth.net>
2017-05-01 14:33:04 -0400

    Added two top level functions to GameObjects, also changed all sprites to inherit from pygame.sprite.Sprite.

--- game.py
+++ game.py
@@ -12,8 +12,10 @@ bg_filenames = ('assets/images/backgrounds/bg_title.jpg',
 # setup game display
 size = screen_width, screen_height = 800, 700
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Penguin')
+screen_rect = screen.get_rect()
+pygame.display.set_caption('Penguin Rescue')
 pygame.display.set_icon(pygame.image.load('assets/images/gui/penguin_icon.png').convert_alpha())
+
 
 # Set Color Constants
 PALE_BLUE = 208, 236, 253
@@ -21,55 +23,48 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 2, 2)
 
-penguin_img = pygame.image.load('assets/images/sprites/small_penguin.png')
-penguin = GameObjects.Penguin(x=40, y=screen_height - 40, width=34, height=34, img=penguin_img)
-
-snowman_img = pygame.image.load('assets/images/sprites/snowman.png')
-snowman = GameObjects.Snowman(width=101, height=72, img=snowman_img)
-
-snowball_img = pygame.image.load('assets/images/sprites/small_snowball.png').convert_alpha()
-snowball = GameObjects.Snowball(width=34, height=34, img=snowball_img)
-
 
 def pquit():
     pygame.quit()
     exit()
 
 
-def game_over():
-    font = pygame.font.SysFont(None, 80)
-    fcolor = pygame.color.Color(5, 5, 5, 100)
-    message = font.render("Game over", True, fcolor)
-    screen.blit(message, (165, 100))
+def draw_sprites(all_sprites):
+    for sprite in all_sprites:
+        screen.blit(sprite.img, (sprite.rect.x, sprite.rect.y))
 
 
-def draw_penguin(penguin):
-    screen.blit(penguin.img, (penguin.x, penguin.y))
+# need a function to update HUD (score, etc)
+def hud(score):
+    pass
 
 
-def update_display(game_stage):
+def update_display(game_stage, all_sprites):
+    screen.fill(PALE_BLUE)
     if game_stage == 'title':
         background = pygame.image.load(bg_filenames[0])
         screen.blit(background, (0, 0))
     elif game_stage == 'game':
         background = pygame.image.load(bg_filenames[1])
         screen.blit(background, (0, 0))
-        draw_penguin(penguin)
+        draw_sprites(all_sprites)
     elif game_stage == 'credits':
         background = pygame.image.load(bg_filenames[2])
         screen.blit(background, (0, 0))
     else:
         print("Error: Unknown game stage.")
         pquit()
+    pygame.display.flip()
 
 
 def game_loop():
-    # update penguin position with keyboard
+    game_stage = 'title'  # starts with the title screen
+
+    player, penguin, yeti, snowball = GameObjects.spawn_sprites(screen_rect)
+    all_sprites = pygame.sprite.RenderUpdates(player, penguin, yeti, snowball)
     x_change = 0
     y_change = 0
-    game_stage = 'title'
     score = 0
-    player_speed = 5
 
     play = True
 
@@ -81,20 +76,26 @@ def game_loop():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     pquit()
+
                 elif game_stage == 'title':
-                    if event.key == pygame.K_SPACE:
+                    if (event.key == pygame.K_SPACE or
+                    event.key == pygame.K_p):
                         game_stage = 'game'
+                    elif event.key == pygame.K_q:
+                        pquit()
+
                 elif game_stage == 'game':
                     if event.key == pygame.K_UP:
-                        y_change -= player_speed
+                        y_change -= player.speed
                     elif event.key == pygame.K_DOWN:
-                        y_change += player_speed
+                        y_change += player.speed
                     elif event.key == pygame.K_LEFT:
-                        x_change -= player_speed
+                        x_change -= player.speed
                     elif event.key == pygame.K_RIGHT:
-                        x_change += player_speed
+                        x_change += player.speed
                     elif event.key == pygame.K_SPACE:
                         game_stage = 'credits'
+
                 elif game_stage == 'credits':
                     pquit()
 
@@ -104,16 +105,33 @@ def game_loop():
                 if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     y_change = 0
 
-        penguin.x += x_change
-        penguin.y += y_change
+        player.rect.x += x_change
+        player.rect.y += y_change
 
-        screen.fill(PALE_BLUE)
-        update_display(game_stage)
-        pygame.display.flip()
-
+        # check for collision with sides of game area
+        # game objects shouldn't be able to go past the sides of the
+        # game area, but hitting it shouldn't end the game either
+        # check for collision with monster
+        # If the player collides with the monster, then the game is over
+        # check for collision with target sprite
+        # the player should get one point for each target sprite rescued(hit)
+        # if (penguin.x <= agent.x <= penguin.x + penguin.width)\
+        #        and (penguin.y <= agent.y <= penguin.y + penguin.height):
+        #    pass
+        # penguin_img = pygame.image.load('assets/images/sprites/small_penguin.png')
+        # penguin = GameObjects.Penguin(width=34, height=34, img=penguin_img)
+        # penguin.x = random.randint(20, (screen_width - 20))
+        # penguin.y = random.randint(120, (screen_height-20))
+        # check for collision with obstacles
+        # obstacles typically just prevent forward movement, but may want to
+        # add the ability later for different effects on the player
+        # like spawning additional monsters, reducing health, score, etc
+        all_sprites.update()
+        update_display(game_stage, all_sprites)
         clock.tick(60)
 
     pquit()
 
 
-game_loop()
+if __name__ == '__main__':
+    game_loop()
